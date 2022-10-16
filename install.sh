0f4610135c1ffc2425ac3208c797a86ef22851e2
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2022-10-16 16:40:42 -0400

    update

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-apt update
-apt -y upgrade
-apt -y full-upgrade
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y full-upgrade
 
-apt install curl mlocate wget default-jdk -y
-apt install git -y
+sudo apt install curl mlocate wget default-jdk -y
+sudo apt install git -y
 
-wget https://dlcdn.apache.org/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
+sudo snap install --classic code
 
-tar xvf spark-3.3.0-bin-hadoop3.tgz
-mv spark-3.3.0-bin-hadoop3/ /opt/spark
+sudo wget https://dlcdn.apache.org/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
 
-echo "export SPARK_HOME=/opt/spark" > ~/.bashrc
-echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" > ~/.bashrc
+sudo tar xvf spark-3.3.0-bin-hadoop3.tgz
+sudo mv spark-3.3.0-bin-hadoop3/ /opt/spark
 
-apt install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt install -y python3.9
-apt-get install -y python3-pip python3-dev
+sudo echo "export SPARK_HOME=/opt/spark" > ~/.bashrc
+sudo echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" > ~/.bashrc
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install -y python3.9
+sudo apt-get install -y python3-pip python3-dev
 
 python3 -m pip install --upgrade pip
 python3 -m pip install setuptools==57.4.0
